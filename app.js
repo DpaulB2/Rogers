ccb947848de2f87273b2fe89c3542197be2e6968
DpaulB2 <149266839+DpaulB2@users.noreply.github.com>
2023-10-28 11:51:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_gkye3ln", "template_9td5grr", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
